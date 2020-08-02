116b045db62f1df3246a592ef06eeb7c2afcc505
Eric Raslich <rasliche@gmail.com>
2020-08-02 16:31:09 -0400

    Clean up and document

--- src/functions/handle-purchase.js
+++ src/functions/handle-purchase.js
@@ -13,18 +13,18 @@ exports.handler = async ({ headers, body }) => {
             headers['stripe-signature'], 
             process.env.STRIPE_WEBHOOK_SECRET)
 
+        // ignore any other kinds of events
         if (event.type !== 'checkout.session.completed') {
-            return {
-                statusCode: 400,
-                body: `Webhook Error: ${error.message}`
-            }
+            return;
         }
 
-        const items = await stripe.checkout.sessions.listLineItems(event.data.object.id);
+        // get out the shipping address and the checkout session ID to retrieve items
+        const { data: { object: { id, shipping } } } = event
 
-        const order = event.data.object
-        
-        // read out the shipping address
+        // retrieve the order items for the email
+        const { data: items } = await stripe.checkout.sessions.listLineItems(id);
+
+        // read out the shipping address parts
         const {
             line1,
             line2,
@@ -32,34 +32,35 @@ exports.handler = async ({ headers, body }) => {
             state,
             postal_code,
             country
-        } = order.shipping.address
+        } = shipping.address
 
         // read out the line items and format email
-
-        
         const msg = {
             to: process.env.FULFILLMENT_EMAIL_ADDRESS,
-            from: process.env.FULFILLMENT_EMAIL_ADDRESS,
-            subject: 'New Order from KeyWestGroundParrot.com!',
+            from: process.env.FROM_EMAIL_ADDRESS,
+            subject: `New Order from KeyWestGroundParrot.com!`,
             text: `
 Items:
-${items.map(item => `- (${item.quantity}) ${item.id}`).join('\n')}
-        
+${items.map(item => `- (${item.quantity}) ${item.description}`).join('\n')}
+
 Shipping Address:
-${order.shipping.name}
+${shipping.name}
 ${line1}${line2 !== null ? '\n' + line2 : ''}
 ${city}, ${state} ${postal_code}
 ${country}
 `,
         };
+        console.log(msg)
         // send email
         await mail.send(msg);
-        
+
+        // let Stripe know everything worked out just fine
         return {
             statusCode: 200,
             body: JSON.stringify({ received: true })
         }
     } catch (error) {
+        // let Stripe know something went wrong
         return {
             statusCode: 400,
             body: `Webhook Error: ${error.message}`
