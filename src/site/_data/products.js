a6626334c6bff61cece3acea9d389ba3c1bea03b
Eric Raslich <rasliche@gmail.com>
2020-08-02 16:30:29 -0400

    Update Product Catalog

--- src/site/_data/products.js
+++ src/site/_data/products.js
@@ -3,20 +3,20 @@ module.exports = products = () => {
     return [
         {
             sku: "KWGP001",
-            name: "Key West Groud Parrot",
-            description: "It's a Key West Ground Parrot. The bird of the Conch Republic.",
-            image: "https://res.cloudinary.com/duzmgsio4/image/upload/v1595558268/keywestgroundparrot.com/key-west-ground-parrot.png",
+            name: "Key West Ground Parrot",
+            description: `It's a Key West Ground Parrot. The bird of the Conch Republic. Ready to take on the world, one Dion's bucket at a time. This sticker is 3"x3" and holographic. Looks great on a water bottle, laptop, tackle box, or truck bumper.`,
+            images: ["https://res.cloudinary.com/duzmgsio4/video/upload/ac_none,c_fill,h_300,q_auto:eco,vc_auto,w_300/v1596398041/keywestgroundparrot.com/383AEF69-9A70-4198-97DC-385CA44923E6.gif", "https://res.cloudinary.com/duzmgsio4/image/upload/w_300,h_300,f_auto,q_auto:eco/v1596398126/keywestgroundparrot.com/kwgp-002.jpg"],
             amount: 300,
             currency: "USD"
         },
-        {
-            sku: "KWGP002",
-            name: "Permit",
-            description: "It's a fish.",
-            image: "https://res.cloudinary.com/duzmgsio4/image/upload/v1595558268/keywestgroundparrot.com/permit.png",
-            amount: 300,
-            currency: "USD"
-        },
+        // {
+        //     sku: "KWGP002",
+        //     name: "Permit",
+        //     description: "It's a fish.",
+        //     image: "https://res.cloudinary.com/duzmgsio4/image/upload/v1595558268/keywestgroundparrot.com/permit.png",
+        //     amount: 300,
+        //     currency: "USD"
+        // },
         // {
         //     sku: "KWGP003",
         //     name: "Flying Fish",
