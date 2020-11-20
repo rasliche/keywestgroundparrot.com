915abff3b73f64804c86ee20c8560f91f2fbaa5b
Eric Raslich <rasliche@gmail.com>
2020-11-20 12:02:36 -0500

    Remove ability to purchase

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function(eleventyConfig) {
   // addPassthroughCopy strips the `dir.input` directory and replaces with `_site`
   eleventyConfig.addPassthroughCopy("src/site/.well-known/brave-rewards-verification.txt");
   eleventyConfig.addPassthroughCopy("src/site/css/*.css");
-  // eleventyConfig.addPassthroughCopy("src/site/js/*.js");
+  eleventyConfig.addPassthroughCopy("src/site/js/*.js");
   eleventyConfig.addPassthroughCopy("src/site/assets/*");
   eleventyConfig.addPassthroughCopy("src/site/service-worker.js");
 
